52353e0cf08b1306b4dd4a1847d64baac21c9442
Khushbu-github <khushbuhajari530@gmail.com>
2025-12-13 18:17:39 +0530

    structure  added

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000';
+
 const instance = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000',
+    baseURL: API_BASE_URL,
 });
 
 // Add a request interceptor
