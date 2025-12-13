b94cbe8498cb4d3998a099ea6a1d6a477de06597
Khushbu-github <khushbuhajari530@gmail.com>
2025-12-13 18:58:14 +0530

    structure  added

--- server/server.js
+++ server/server.js
@@ -11,7 +11,15 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    "https://www.autofreaks.in",
+    "https://autofreaks.in",
+    "https://api.autofreaks.in",
+    "http://localhost:5173"
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 // ✅ Base Route
